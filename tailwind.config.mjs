393dd6054d46fbb44d0bf54963f67f0b7deef908
Matt Jewell <m@mattjewell.co.uk>
2024-11-23 17:17:27 +0100

    fonts and shit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,18 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["system-ui", "sans-serif"],
+      serif: [
+        "Didot",
+        "Bodoni MT",
+        "Noto Serif Display",
+        "URW Palladio L",
+        "P052",
+        "Sylfaen",
+        "serif",
+      ],
+    },
     extend: {},
   },
   plugins: [],
